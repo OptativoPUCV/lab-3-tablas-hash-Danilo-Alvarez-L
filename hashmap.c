df24ce50470d4e051069e51ca3ce847f0eae927c
Your Name <you@example.com>
2025-04-20 16:44:34 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -119,12 +119,30 @@ Pair * searchMap(HashMap * map,  char * key)
     return NULL;
 }
 
-Pair * firstMap(HashMap * map) {
+Pair * firstMap(HashMap * map)
+{
+
+    for (long i = 0 ; i < map->capacity ; i++)
+    {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL )
+        {
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
 
     return NULL;
 }
 
-Pair * nextMap(HashMap * map) {
-
+Pair * nextMap(HashMap * map)
+{
+    for (long i = map->current ; i < map->capacity ; i++)
+    {
+        if (map->buckets[i] != NULL && map->buckets[i]->key != NULL )
+        {
+            map->current = i;
+            return map->buckets[i];
+        }
+    }
     return NULL;
 }
